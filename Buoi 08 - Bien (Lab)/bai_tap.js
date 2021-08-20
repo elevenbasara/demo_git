7b51f10691d1bf79bcb02894a49bbe658784437b
elevenbasara <eleven_basara@yahoo.com.vn>
2021-08-20 10:25:21 +0700

    add Buoi 9

--- Buoi 08 - Bien (Lab)/bai_tap.js
+++ Buoi 08 - Bien (Lab)/bai_tap.js
@@ -1,19 +1,19 @@
 const PI = 3.14;
-function chu_vi_hinh_tron(){
+function chu_vi_hinh_tron() {
     let bankinh;
     bankinh = +document.getElementById('bankinh').value;
-    chuvi = 2*bankinh*PI;
+    chuvi = 2 * bankinh * PI;
     document.getElementById('result-chuvi').innerHTML = chuvi;
 }
 
-function dien_tich_hinh_tron(){
+function dien_tich_hinh_tron() {
     let bankinh;
     bankinh = +document.getElementById('bankinh').value;
-    dientich = bankinh*bankinh*PI;
+    dientich = bankinh * bankinh * PI;
     document.getElementById('result-dientich').innerHTML = dientich;
 }
 
-function buc_thu(){
+function buc_thu() {
     let ten, diachi, ngay, thang, nam;
     ten = document.getElementById('ten').value;
     diachi = document.getElementById('diachi').value;
@@ -22,5 +22,30 @@ function buc_thu(){
     nam = document.getElementById('nam').value;
     document.getElementById('modau').innerHTML = ten + ' thương nhớ,';
     document.getElementById('noidung').style.display = 'block';
-    document.getElementById('cuoi').innerHTML = diachi +', ' + 'ngày ' + ngay + ' tháng ' + thang + ' năm ' + nam + '.'; 
+    document.getElementById('cuoi').innerHTML = diachi + ', ' + 'ngày ' + ngay + ' tháng ' + thang + ' năm ' + nam + '.';
 }
+
+function convert_currency(){
+    let money, from, to;
+    money = +document.getElementById('money').value;
+    from = +document.getElementById('from').value;
+    to = +document.getElementById('to').value;
+    result = (money*to)/from
+    document.getElementById('convert-result').innerHTML = 'Result là ' + result;
+    /* cách làm nếu đặt option = vnd or usd
+    if(from === 'vnd'){
+        if(to === 'vnd'){
+            document.getElementById('convert-result').innerHTML = 'Result là ' + money + ' VND';
+        } else{
+            document.getElementById('convert-result').innerHTML = 'Result là ' + (money/23000) + ' USD';
+        }
+    } else{
+        if(to === 'usd'){
+            document.getElementById('convert-result').innerHTML = 'Result là ' + money + ' USD';
+        } else{
+            document.getElementById('convert-result').innerHTML = 'Result là ' + (money*23000) + ' VND';
+        }
+    }
+    */
+}
+
---
+++ Buoi 09 - Nhap xuat du lieu/bai_tap.js
@@ -0,0 +1,66 @@
+function year(){
+    let birthday = document.getElementById('birthday').value;
+    let date = new Date(birthday);
+    birthdayYear = date.getFullYear();
+    let now = new Date();
+    nowYear = now.getFullYear();
+    if(date.getMonth() == now.getMonth()){
+        if(date.getDate() >= now.getDate()){
+            document.getElementById('result-year').innerHTML = 'Số tuổi hiện tại là ' + (nowYear - birthdayYear - 1) + ' tuổi' 
+        } else{
+            document.getElementById('result-year').innerHTML = 'Số tuổi hiện tại là ' + (nowYear - birthdayYear) + ' tuổi' 
+        }
+    } if (date.getMonth() < now.getMonth()){
+        document.getElementById('result-year').innerHTML = 'Số tuổi hiện tại là ' + (nowYear - birthdayYear) + ' tuổi' 
+    } if (date.getMonth() > now.getMonth()){
+        document.getElementById('result-year').innerHTML = 'Số tuổi hiện tại là ' + (nowYear - birthdayYear - 1) + ' tuổi' 
+    }
+}
+
+function may_tinh_phen(){
+    const PHEP_CHIA = '/';
+    let so_thu_1, so_thu_2, toan_tu;
+    so_thu_1 = document.getElementById('so_thu_1').value;
+    so_thu_2 = document.getElementById('so_thu_2').value;
+    toan_tu = document.getElementById('toan_tu').value;
+    if(toan_tu == PHEP_CHIA){
+        if(so_thu_2 == 0){
+            alert('Không có phép chia cho 0');
+            return; //kết thúc chương trình
+        }
+    }
+    result = eval(so_thu_1 + toan_tu + so_thu_2);  // hàm eval tính toán biểu thức dạng string
+    document.getElementById('result-may-tinh-phen').innerHTML = result;
+}
+
+//máy tính
+
+function setInput(value){
+    document.getElementById('input').value += value;
+}
+
+function calculator(){
+    let input;
+    input = document.getElementById('input').value;
+    result = eval(input);
+    document.getElementById('input').value = result;
+}
+
+function calculatorMinus(){
+    let input;
+    input = document.getElementById('input').value;
+    result = eval(input * -1);
+    document.getElementById('input').value = result;
+}
+
+function calculatorPercent(){
+    let input;
+    input = document.getElementById('input').value;
+    result = eval(input * 0.01);
+    document.getElementById('input').value = result;
+}
+
+function clearInput(){
+    document.getElementById('input').value = '';
+}
+
